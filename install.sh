0b0582cdec725a60faf171c3f2297ab5e159bf5e
vicevalds <vicentevaldesfigueroa@protonmail.com>
2023-02-02 17:13:23 -0300

    install.sh fix .config paste

--- install.sh
+++ install.sh
@@ -5,17 +5,25 @@ redColour="\e[0;31m\033[1m"
 
 if [ $# -eq 1 ]; then
 	if [ $(ls /home | grep -w "${1}") ]; then
-		if [ $USER -eq root 2&>/dev/null ]; then
+		if [ $USER ]; then
+			
+                        #root
+                        cp -r mv/root_home/.[cz]* /root/
+
+                        usermod --shell /bin/zsh root
+
+                        #$USER
+                        cp -r .[cz]* /home/$1/
+                        chown -R $1:$1 /home/$1/.[cz]*
+
+                        usermod --shell /bin/zsh $1
+
 			#Pacman 
 			pacman -S pacman-contrib qtile lightdm lightdm-webkit2-greeter kitty rofi redshift picom feh zsh bat mdcat lsd locate cbatticon pulseaudio pavucontrol volumeicon brightnessctl playerctl udiskie ntfs-3g network-manager-applet imagemagick thunar neofetch vlc scrot i3lock wget p7zip python-pip pacman-contrib libnotify notification-daemon
 
 			#Paru
 			paru -S zsh-autosuggestions zsh-syntax-highlighting
 
-			echo -e "\n\n\t[!] Utilidades\n"
-
-			paru -S librewolf-bin
-		
 			systemctl enable lightdm.service
 	
 			git clone https://github.com/vicevalds/lightdm-webkit2-gif-theme /usr/share/lightdm-webkit/themes/
@@ -29,17 +37,6 @@ if [ $# -eq 1 ]; then
 			cp mv/lock.sh /opt/
 			cp mv/onedark.rasi /usr/share/rofi/themes/
 			cp -r mv/zsh-sudo /usr/share/zsh/plugins/
-
-			#root
-			cp -r mv/root_home/.[cz]* /root/
-
-			usermod --shell /bin/zsh root
-
-			#$USER
-			cp -r .[cz]* /home/$1/
-			chown -R $1:$1 /home/$1/.[cz]*
-
-			usermod --shell /bin/zsh $1
 
 			sudo localectl set-x11-keymap latam
 
